31b042eac9aba1e570690a6884f3f5cae8071e75
German1127 <kleverk2023@gmail.com>
2024-05-02 11:07:20 -0700

    fix task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
+// script
+
+const argvs = process.argv.slice(2);
+const filename = argvs[0];
+const content = argvs[1];
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const string = process.argv[3];
-
-fs.writeFile(filePath, string, 'utf-8', (err) => {
+fs.writeFile(filename, content, err => {
   if (err) {
-    console.log(err);
+    console.error(err);
   }
 });
