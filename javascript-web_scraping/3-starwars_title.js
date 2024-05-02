514d74a1494f6dd666c2379a8e55dc7ae273734d
German1127 <kleverk2023@gmail.com>
2024-05-02 13:32:38 -0700

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = 'https://swapi-api.hbtn.io/api/films/:id';
 request.get(url + id, (err, response, body) => {
   if (err) {
     console.log(err);
