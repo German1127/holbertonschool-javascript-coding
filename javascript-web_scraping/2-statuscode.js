816c7faae9dbfc588113b00ce11d5b286252b78e
German1127 <kleverk2023@gmail.com>
2024-05-02 13:24:10 -0700

    fix task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,18 +2,11 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error("Usage: node script.js <URL>");
-  process.exit(1);
-}
-
 const url = process.argv[2];
-
-request(url, function (err, response, body) {
+request.get(url, (err, response) => {
   if (err) {
-    console.error("Error making request:", err);
-    process.exit(1);
+    console.log(err);
   } else {
-    console.log('code:', response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
